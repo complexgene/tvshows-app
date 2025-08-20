6817ec55cf5ed5e80f553bb4955be7f5d7e6d1a2
sanaskar007 <santanu.code@gmail.com>
2025-08-21 01:06:41 +0530

    API docs

--- tvshows-be/src/main/java/com/avatarconnex/tvshowsbe/controller/TVShowController.java
+++ tvshows-be/src/main/java/com/avatarconnex/tvshowsbe/controller/TVShowController.java
@@ -4,7 +4,10 @@ import com.avatarconnex.tvshowsbe.models.AppResponse;
 import com.avatarconnex.tvshowsbe.models.ShowListItemDto;
 import com.avatarconnex.tvshowsbe.models.TVShowDetails;
 import com.avatarconnex.tvshowsbe.service.TVShowService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -22,12 +25,14 @@ public class TVShowController {
     @Autowired
     private TVShowService tvShowService;
 
-    @GetMapping
+    @GetMapping("/all")
+    @Operation(summary = "[Non-Paginated] List all TV Shows", description = "Fetches non-paginated list of TV shows")
     public List<TVShowDetails> getAllTvShows() {
         return tvShowService.fetchAllTvShows();
     }
 
     @GetMapping("/paged")
+    @Operation(summary = "[Paginated] List all TV Shows", description = "Paginated list of TV shows available in the system")
     public ResponseEntity<Page<ShowListItemDto>> getAllTvShowsPaged(
             @PageableDefault(size = 20, sort = "name", direction = Sort.Direction.ASC) Pageable pageable) {
         Page<ShowListItemDto> tvShows = tvShowService.fetchAllTVShows(pageable);
@@ -35,13 +40,15 @@ public class TVShowController {
     }
 
     @PostMapping("/import")
+    @Operation(summary = "Import details of TV Shows", description = "Reads the given file and fetches the TV show details from external API")
     public ResponseEntity<AppResponse> importTvShows() {
         AppResponse appResponse = tvShowService.importShowDataFromTitles();
         return ResponseEntity.ok(appResponse);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<TVShowDetails> getAllTvShowsPaged(@PathVariable("id") Long showId) {
+    @Operation(summary = "Get TV Show by ID", description = "Fetch details of a TV show using its ID")
+    public ResponseEntity<TVShowDetails> getTvShowById(@PathVariable("id") Long showId) {
         TVShowDetails tvShowDetails = tvShowService.fetchShowDetail(showId);
         return ResponseEntity.ok(tvShowDetails);
     }
